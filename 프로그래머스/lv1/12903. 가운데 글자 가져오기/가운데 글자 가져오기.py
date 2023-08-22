ea487c10f71d277a944b153c44ec7a021620281f
Jeongyun <131102119+jeongyun-kim@users.noreply.github.com>
2023-08-22 11:24:41 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = ''
+    length = len(s)
+    mid = length//2
+
+    if length % 2 == 0 :
+        answer = s[mid-1:mid+1]
+    else :
+        answer = s[mid]
+        
+    return answer
