2ee5fc10e483053a9ec081dcf73ee9db3f2f9c80
Jeongyun <131102119+jeongyun-kim@users.noreply.github.com>
2023-10-30 21:05:52 +0900

    [level unrated] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ num_list:[Int]) -> Int {
+    var answer: Int = -1
+    
+    for i in 0..<num_list.count {
+        if num_list[i] < 0 {
+            answer = i
+            break
+        }
+    }
+    
+    return answer
+}
