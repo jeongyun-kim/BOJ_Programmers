32f4db373c26de64a708e20fbb12a2127aa10a8d
Jeongyun <131102119+jeongyun-kim@users.noreply.github.com>
2024-02-15 16:43:07 +0900

    [Silver I] Title: 보석 상자, Time: 2912 ms, Memory: 42876 KB -BaekjoonHub

--- 백준/Silver I/2792. 보석 상자/보석 상자.py
+++ 백준/Silver I/2792. 보석 상자/보석 상자.py
@@ -13,14 +13,14 @@ end = sum(jewels) # 한 명이 가져갈 수 있는 최대 개수
 while start <= end :
     mid = (start + end) // 2
     students = 0 # 보석을 가져간 사람 수 
-    for jewel in jewels :
-        if jewel % mid == 0 :
+    for jewel in jewels : # 각 보석의 개수를 돌면서 
+        if jewel % mid == 0 : # 보석을 공평하게 배분할 수 있으면  
             students += (jewel // mid)
-        else :
+        else : # 보석을 공평하게 배분할 수 없으면 
             students += (jewel // mid + 1)
-    if students > n :
-        start = mid + 1
-    else :
-        end = mid - 1
+    if students > n : # 보석을 배분받은 학생의 수가 주어진 학생보다 많으면 
+        start = mid + 1 # 인당 가져갈 수 있는 보석의 개수 증가 
+    else : # 보석을 배분받은 학생의 수가 주어진 학생보다 적으면
+        end = mid - 1 # 인당 가져갈 수 있는 보석의 개수 감소 
 
 print(end+1)
