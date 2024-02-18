85524e3438d7a4f376b73ef1efa66142df7b7542
Jeongyun <131102119+jeongyun-kim@users.noreply.github.com>
2024-02-18 17:41:11 +0900

    [Silver IV] Title: 수들의 합 2, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,19 +1,28 @@
 # 수열의 i번째부터 j번째까지의 합이 M이 되는 경우의 수
 # n : 수열의 길이, m : 만들어야 하는 수
-# sum >= m : i + 1
+# sum > m : i + 1
 # sum < m : j + 1
+# sum == m : i + 1, answer + 1
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-j = 0
-current_sum = 0 # i부터 j까지의 합 
+i = 0
+j = 1
+current_sum = arr[0]
 answer = 0
 
-for num in arr :
-    while current_sum < m and j < n : 
-        current_sum += arr[j]
-        j += 1
-    if current_sum == m :
+while (1) :
+    if current_sum < m :
+        if j < n :
+            current_sum += arr[j]
+            j += 1
+        else :
+            break 
+    elif current_sum == m :
         answer += 1
-    current_sum -= num # A[0] + A[1]의 상태에서 A[0]값빼주면 i가 +1 된 것과 같음 
-    
+        current_sum -= arr[i]
+        i += 1
+    else :
+        current_sum -= arr[i]
+        i += 1
+
 print(answer)
