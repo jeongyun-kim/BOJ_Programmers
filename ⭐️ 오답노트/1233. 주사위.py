d6b97866c7bebf1522f16b823f296a43fd7ffc2c
Jeongyun <131102119+jeongyun-kim@users.noreply.github.com>
2024-03-24 22:10:46 +0900

    Update 1233. 주사위.py

--- ⭐️ 오답노트/1233. 주사위.py
+++ ⭐️ 오답노트/1233. 주사위.py
@@ -1,13 +1,17 @@
-# S1 : 2~20, S2 : 2~20, S3 : 2~40
-# 세 주사위를 동시에 던져 가장 높은 빈도로 나오는 세 주사위의 합을 구하기 
-arr = [0]*81 # 최대 합이 80이기 때문에 배열 길이는 81
-s1, s2, s3 = map(int, input().split())
+# 백준 1233 주사위
 
-for i in range(1, s1+1) :
+# 주사위 S1, S2(2이상 20이하) / S3(2이상 40이하)의 주사위들을 동시에 던져
+# 가장 높은 빈도로 나오는 세 주사위의 합 구하기 
+# S1, S2, S3이 입력으로 주어짐
+s1, s2, s3 = map(int, input().split())
+nums = [0] * (s1+s2+s3+1) # 세 주사위의 면을 합쳐서 나오는 수의 횟수 
+
+# 세 주사위 면의 수 합쳐서 세기 
+for i in range(1, s1+1) : 
     for j in range(1, s2+1) :
         for k in range(1, s3+1) :
-            arr[i+j+k] += 1
+            nums[i+j+k] += 1
 
-# 정답이 여러개라면 가장 합이 작은 것
-# index의 경우 일치하는 항목 중 가장 앞의 것을 가져오기 때문에 조건에 부합
-print(arr.index(max(arr)))
+# 가장 많이 나오는 수 출력하기 
+max_num = max(nums)
+print(nums.index(max_num))
